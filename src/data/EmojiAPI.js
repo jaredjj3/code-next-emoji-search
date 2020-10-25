1f5028b1fb742bfacdb61616b1501dbd86066157
Jared Johnson <jared@jaredjohnson.dev>
2020-10-25 16:09:17 -0400

    Add solution and remove solution from src

--- src/data/EmojiAPI.js
+++ src/data/EmojiAPI.js
@@ -1,10 +1,3 @@
 import ALL_EMOJIS from './ALL_EMOJIS.json';
 
-export const search = (query, limit) => {
-  query = query.toLowerCase();
-  return ALL_EMOJIS.filter(emoji => {
-    const title = emoji.title.toLowerCase();
-    const keywords = emoji.keywords.toLowerCase();
-    return title.includes(query) || keywords.includes(query);
-  }).slice(0, limit);
-};
+export const search = (query, limit) => {};
